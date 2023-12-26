222a273e7b6431e3808ed55dfe9db14903e39622
Marcelo Vieira Gonçalves <136708392+Vieirajrz7@users.noreply.github.com>
2023-12-26 12:38:18 -0300

    Jogo de "LABIRINTO" 2.0
    Jogo de labirinto simples feito com HTML, CSS e JS

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function gameOverDir(img) {
+    document.getElementById("imgSusto").style.display = 'block';
+    document.getElementById("reset").style.display = 'block';
+}
+
+function gameOverEsq(img) {
+    document.getElementById("imgSusto").style.display = 'block'; 
+    document.getElementById("reset").style.display = 'block';
+}
+
+function susto() {
+    let x = document.getElementById("susto");
+    x.onplay();
+}
+
+function ganhou() {
+    document.getElementById("ladoDireito").style.display = 'none';
+    document.getElementById("ladoEsquerdo").style.display = 'none';
+    document.getElementById("ganhou").innerHTML = "VOCÊ GANHOU :)";
+    document.getElementById("reset").style.display = 'block';
+   
+}
+
+function comecar() {
+    document.getElementById("tudo").style.display = 'block';
+    document.getElementById("comecar").style.display = 'none';
+}
